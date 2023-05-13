e1258f15a8bc1868257c9d664a562896130dd569
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-14 00:18:05 +0900

    first

---
+++ src/main/java/eunhye/GooGoo/GooGooApplication.java
@@ -0,0 +1,13 @@
+package eunhye.GooGoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooGooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooGooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eunhye/GooGoo/GooGooApplicationTests.java
@@ -0,0 +1,13 @@
+package eunhye.GooGoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooGooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
